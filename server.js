77e7c0fe9aef653d6d06e00ec911582f3c69504d
System <none>
2017-01-10 08:24:49 +0000

    🍁😱 Updated with Gomix

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var port = process.env.PORT;
+var app = express();
+var urls = [];
+app.get('/',(req, res) => {
+  res.send('use /:url to set a new one, /goto/:tinyurl to get the original one');
+});
+
+app.get('/:url', (req, res) => {
+  var url = req.params.url;
+  if(url.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g)){
+    var n = {short: 'doesntexist.itreallydoesnt' + urls.length, original: url};
+    urls.push(n);
+    res.json(n);
+  }else{
+    res.json({error: 'The url is invalid', url: url});
+  }
+});
+app.get('/goto/:tinyurl', function(req, res) {
+    urls.some((v) => {
+      if(v.short === req.params.tinyurl){
+        res.redirect('http://' + v.original);
+        return true;
+      }return false;
+    });
+});
+app.listen(port, console.log(`Listening on port ${port}`));
